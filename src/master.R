1925c7c4b0c7b4ffccd0275a28981b95923ca3cf
Nick Dawe <nick@towananalytics.com>
2021-10-13 14:24:30 +0800

    reference docs added

--- src/master.R
+++ src/master.R
@@ -54,10 +54,6 @@ water_level <- simulated_water_level(start_date_time = start_date_time,
                                      time_interval = 20, #minutes
                                      days_of_readings = 1)
 
-# hms::as_hms(time) >= hms::as_hms(min(hms::as_hms(time_constraint))) &
-#   hms::as_hms(time) <= hms::as_hms(max(hms::as_hms(time_constraint)))
-
-
 # FOR LOOP START HERE -----------------------------------------------------
 
 # Initialise the dredge position
@@ -73,7 +69,7 @@ temp <- matrix(ncol = 4,
 temp[1, 1] <- start_date_time
 temp[1, 2] <- 0 #m3
 
-dredge.buckets.complete <- 0
+dredge.buckets.complete <- 0 # Increments as buckets are extracted
 
 # Start with the first hopper alongside and then fill with dredger --------
 
@@ -87,15 +83,15 @@ for(j in 1:ncol(seabed_surface)){ # cells along the easting axis (columns) - sta
     easting <- as.numeric(colnames(seabed_surface)[j])
     
     hopper_details <- hopper_split(
-      number_of_hoppers = 2,
-      hopper_capacity = c(250, 130), # m3 NOTE - Vector lengths must match the number of hoppers 
-      hopper_efficiency = c(0.8, 0.8),
-      sailing_distance = 15, #km to disposal ground
-      discharge_time = c(15, 45), #seconds
-      sail_spd_full = c(6, 12), #knots
-      sail_spd_empty = c(6, 12), #knots
-      attach_line_time = c(15, 30)
-    )
+                                    number_of_hoppers = 2,
+                                    hopper_capacity = c(250, 130), # m3 NOTE - Vector lengths must match the number of hoppers 
+                                    hopper_efficiency = c(0.8, 0.8),
+                                    sailing_distance = 15, #km to disposal ground
+                                    discharge_time = c(15, 45), #seconds
+                                    sail_spd_full = c(6, 12), #knots
+                                    sail_spd_empty = c(6, 12), #knots
+                                    attach_line_time = c(15, 30)
+                                    )
     
     # for(x in seq_along(hopper_details[, 1])){
       
@@ -106,40 +102,54 @@ for(j in 1:ncol(seabed_surface)){ # cells along the easting axis (columns) - sta
       
       
       
+      if(hms::as_hms(t) >= hms::as_hms(min(hms::as_hms(time_constraint))) &
+         hms::as_hms(t) <= hms::as_hms(max(hms::as_hms(time_constraint)))) { # Operating within operational hours
+      
       for(y in 1:total.buckets.in.cell){ # Keep dredging up to total number of buckets for cell
         
         # Dredge at cell position: ------------------------------------------------
         # Calculate volume at cell location
-    
-        depth <- water_depth( loc_north = northing,
-                              loc_east = easting,
-                              water_levels = water_level,
-                              current_time = time,
-                              bed_grid = seabed_surface
-        )
-        
-        time <- dredge_cycle_time(water_depth_at_location = depth, 
-                                  bucket_size = bucket_size,
-                                  boom_length = boom_length,
-                                  bucket_cut_depth = bucket_cut_depth,
-                                  bucket_fill_efficiency = bucket_fill_efficiency,
-                                  bucket_emptying_time = bucket_emptying_time,
-                                  hopper_height_above_water = hopper_height_above_water,
-                                  slew_rate = slew_rate,
-                                  slew_angle = slew_angle,
-                                  bucket_speed = bucket_speed,
-                                  bucket_speed_cutting = bucket_speed_cutting,
-                                  material_bulk_factor = material_bulk_factor
-        ) + start_date_time
-        
-        cut.vol <- bucket_size * bucket_fill_efficiency
-        
-        print(time)
         
         dredge.buckets.complete <- dredge.buckets.complete + 1
         
-        temp[dredge.buckets.complete, 1] <- time # 1 + y to offset from the initialised start point
-        temp[dredge.buckets.complete, 2] <- cut.vol
+        if(sum(temp[n:dredge.buckets.complete, 2], na.rm = TRUE) < hopper_actual_capacity) { # Check if still space in the hopper dredge
+          
+          # TODO n not defined for above if statement.
+          # TODO insert dredge functions here when there is space in the hopper
+    
+          depth <- water_depth( loc_north = northing,
+                                loc_east = easting,
+                                water_levels = water_level,
+                                current_time = time,
+                                bed_grid = seabed_surface
+          )
+          
+          time <- dredge_cycle_time(water_depth_at_location = depth, 
+                                    bucket_size = bucket_size,
+                                    boom_length = boom_length,
+                                    bucket_cut_depth = bucket_cut_depth,
+                                    bucket_fill_efficiency = bucket_fill_efficiency,
+                                    bucket_emptying_time = bucket_emptying_time,
+                                    hopper_height_above_water = hopper_height_above_water,
+                                    slew_rate = slew_rate,
+                                    slew_angle = slew_angle,
+                                    bucket_speed = bucket_speed,
+                                    bucket_speed_cutting = bucket_speed_cutting,
+                                    material_bulk_factor = material_bulk_factor
+          ) + start_date_time
+          
+          cut.vol <- bucket_size * bucket_fill_efficiency
+          
+          print(time)
+          
+          temp[dredge.buckets.complete, 1] <- time # 1 + y to offset from the initialised start point
+          temp[dredge.buckets.complete, 2] <- cut.vol
+          
+        } else { 
+          
+          # TODO Iterate to next available hopper
+          
+        }
         
         # temp[1 + y, 1] <- time # 1 + y to offset from the initialised start point
         # temp[1 + y, 2] <- cut.vol
@@ -147,13 +157,18 @@ for(j in 1:ncol(seabed_surface)){ # cells along the easting axis (columns) - sta
         start_date_time <- time
         
       # } # Then move dredge to next cell
-
-    } # Cell complete - move dredge to next cell
+        
+      } # Cell complete - move dredge to next cell
+      }
   }
-}
+  }
 
 temp.df <- as.data.frame(temp)
 
 temp.df[, 1] <- as.POSIXct(temp.df[,1], origin = "1970-01-01 00:00:00 ", tz = "Australia/Perth")
+ # Dredge duration
+duration <- as.numeric(max(temp.df$V1, na.rm = TRUE) - min(temp.df$V1, na.rm = TRUE))
+
+m3_per_hr <- sum(temp.df$V2, na.rm = TRUE) / as.numeric(duration)
 
 # tatools::save_xlsx(data = c("hopper_details", "temp.df"))
